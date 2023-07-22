d74dc56ba10dd23682441e27897d8c510bf9959e
dddddgz <1978563501@qq.com>
2023-07-22 22:01:54 +0800

    7-22-1

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 pygame.init()
 
-引力常数 = 6
+########## 可更改：引力的大小 ##########
+引力常数 = 1
 
 class 天体:
     def __init__(self, 名称, x, y, x速度, y速度, 质量):
@@ -27,47 +28,67 @@ class 天体动画精灵(pygame.sprite.Sprite):
         self.刷新rect属性()
 
     def 刷新rect属性(self):
-        self.rect.topleft = self.天体.x, self.天体.y
+        self.rect.centerx = self.天体.x
+        self.rect.centery = self.天体.y
 
 def 移动全部天体(时间):
-    for 天体sprite1 in 天体动画精灵列表:
-        天体1 = 天体sprite1.天体
+    要删除的天体 = []
+    for 天体精灵1 in 天体动画精灵列表:
+        天体1 = 天体精灵1.天体
+        if 天体1 in 要删除的天体:
+            continue
         x1, y1, x速度1, y速度1, 质量1 = 天体1.基本信息
         x加速度1, y加速度1 = 0, 0
-        for 天体2 in 天体动画精灵列表:
-            天体2 = 天体2.天体
-            if 天体1 != 天体2:
-                x2, y2, x速度2, y速度2, 质量2 = 天体2.基本信息
-                x距离 = x2 - x1
-                y距离 = y2 - y1
-                距离 = (x距离 ** 2 + y距离 ** 2) ** 0.5
-                力 = 引力常数 * 质量1 * 质量2 / (距离 ** 2)
-                加速度 = 力 / 质量1
-                x加速度1 += 加速度 * (x距离 / 距离)
-                y加速度1 += 加速度 * (y距离 / 距离)
+        for 天体精灵2 in 天体动画精灵列表:
+            天体2 = 天体精灵2.天体
+            if 天体2 in 要删除的天体:
+                continue
+            if 天体1 is 天体2:
+                continue
+            x2, y2, x速度2, y速度2, 质量2 = 天体2.基本信息
+            x距离 = x2 - x1
+            y距离 = y2 - y1
+            距离 = (x距离 ** 2 + y距离 ** 2) ** 0.5
+            if 距离 < 50:
+                # 太近了
+                if 天体1.质量 > 天体2.质量:
+                    要删除的天体.append(天体2)
+                else:
+                    要删除的天体.append(天体1)
+                continue
+            力 = 引力常数 * 质量1 * 质量2 / (距离 ** 2)
+            加速度 = 力 / 质量1
+            x加速度1 += 加速度 * (x距离 / 距离)
+            y加速度1 += 加速度 * (y距离 / 距离)
         x, y = (
             x1 + x速度1 * 时间 + 0.5 * x加速度1 * (时间 ** 2),
             y1 + y速度1 * 时间 + 0.5 * y加速度1 * (时间 ** 2)
         )
-        天体1.x = x
-        天体1.y = y
-        天体sprite1.刷新rect属性()
+        临时x, 临时y = 天体1.x, 天体1.y
+        天体1.x, 天体1.y = x, y
+        天体1.x速度 = (x - 临时x) / 时间
+        天体1.y速度 = (y - 临时y) / 时间
+        天体精灵1.刷新rect属性()
+    for 动画精灵 in 天体动画精灵列表:
+        if 动画精灵.天体 in 要删除的天体:
+            天体动画精灵列表.remove(动画精灵)
 
-screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+screen = pygame.display.set_mode((900, 900))
 
 天体动画精灵列表 = [
-    天体动画精灵(天体("planet1", 0, 0, 1, 1, 10), 50, "orange"),
-    天体动画精灵(天体("planet2", 5, 5, -1, -1, 5), 10, "blue")
+    天体动画精灵(天体("planet1", 500, 100, 1, 0, 1e4), 50, "orange"),
+    天体动画精灵(天体("planet2", 500, 900, -1, 0, 1e4), 50, "blue")
 ]
 
 clock = pygame.time.Clock()
 running = True
 while running:
     screen.fill((0, 0, 0))
-    clock.tick(30)
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    ########## 可更改：天体移动速度 ##########
     移动全部天体(1)
     for 天体动画精灵 in 天体动画精灵列表:
         screen.blit(天体动画精灵.image, 天体动画精灵.rect)
