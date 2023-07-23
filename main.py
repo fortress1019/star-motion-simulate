101bccc697ddd8fa1b6bad76c35d4c91f53f10e6
dddddgz <1978563501@qq.com>
2023-07-23 11:25:12 +0800

    7-23-1

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 pygame.init()
 
 ########## 可更改：引力的大小 ##########
-引力常数 = 1
+引力常数 = 10
 
 class 天体:
     def __init__(self, 名称, x, y, x速度, y速度, 质量):
@@ -31,6 +31,17 @@ class 天体动画精灵(pygame.sprite.Sprite):
         self.rect.centerx = self.天体.x
         self.rect.centery = self.天体.y
 
+def get距离(天体精灵1, 天体精灵2):
+    x1, y1, x速度1, y速度1, 质量1 = 天体精灵1.天体.基本信息
+    x2, y2, x速度2, y速度2, 质量2 = 天体精灵2.天体.基本信息
+    x距离 = x2 - x1
+    y距离 = y2 - y1
+    return (x距离 ** 2 + y距离 ** 2) ** 0.5
+
+def is碰撞(天体精灵1, 天体精灵2):
+    半径1, 半径2 = 天体精灵1.image.get_width(), 天体精灵2.image.get_height()
+    return 半径1 + 半径2 > get距离(天体精灵1, 天体精灵2)
+
 def 移动全部天体(时间):
     要删除的天体 = []
     for 天体精灵1 in 天体动画精灵列表:
@@ -48,13 +59,13 @@ def 移动全部天体(时间):
             x2, y2, x速度2, y速度2, 质量2 = 天体2.基本信息
             x距离 = x2 - x1
             y距离 = y2 - y1
-            距离 = (x距离 ** 2 + y距离 ** 2) ** 0.5
-            if 距离 < 50:
-                # 太近了
-                if 天体1.质量 > 天体2.质量:
-                    要删除的天体.append(天体2)
-                else:
-                    要删除的天体.append(天体1)
+            距离 = get距离(天体精灵1, 天体精灵2)
+            if is碰撞(天体精灵1, 天体精灵2):
+                重天体 = 天体精灵1 if 天体1.质量 > 天体2.质量 else 天体精灵2
+                轻天体 = 天体精灵1 if 重天体 is 天体2 else 天体精灵2
+                要删除的天体.append(轻天体)
+                重天体.天体.x速度 += 轻天体.天体.x速度
+                重天体.天体.y速度 += 轻天体.天体.y速度
                 continue
             力 = 引力常数 * 质量1 * 质量2 / (距离 ** 2)
             加速度 = 力 / 质量1
@@ -70,21 +81,22 @@ def 移动全部天体(时间):
         天体1.y速度 = (y - 临时y) / 时间
         天体精灵1.刷新rect属性()
     for 动画精灵 in 天体动画精灵列表:
-        if 动画精灵.天体 in 要删除的天体:
+        if 动画精灵 in 要删除的天体:
             天体动画精灵列表.remove(动画精灵)
 
 screen = pygame.display.set_mode((900, 900))
 
 天体动画精灵列表 = [
-    天体动画精灵(天体("planet1", 500, 100, 1, 0, 1e4), 50, "orange"),
-    天体动画精灵(天体("planet2", 500, 900, -1, 0, 1e4), 50, "blue")
+    天体动画精灵(天体("planet1", 450, 50, 1, 3, 1000), 10, "green"),
+    天体动画精灵(天体("planet2", 450, 850, -2, -1, 1000), 10, "blue"),
+    天体动画精灵(天体("planet3", 50, 450, 1, -2, 1000), 10, "cyan")
 ]
 
 clock = pygame.time.Clock()
 running = True
 while running:
     screen.fill((0, 0, 0))
-    clock.tick(60)
+    clock.tick(30)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
